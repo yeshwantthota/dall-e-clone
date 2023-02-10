5b5e10c1cf810208f3458eca1575f0b9f3c301bc
Yeshwant Thota <thotayeshwant@gmail.com>
2023-02-10 19:10:24 +0530

    render links added

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -21,12 +21,15 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:8080/api/v1/post", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const response = await fetch(
+        "https://dall-e-4dym.onrender.com/api/v1/post",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       if (response.ok) {
         const result = await response.json();
