4a27ae8cbf2894c374b1914c7a41dffd5cf85c4a
Yeshwant Thota <thotayeshwant@gmail.com>
2023-02-10 20:34:45 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
